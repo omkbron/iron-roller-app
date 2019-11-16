0bdb4d6bfe301972b400988228cf07dd0a800eae
Omar Velasco <omvp29@gmail.com>
2019-11-16 12:07:58 -0600

    Se agrega pagina para carga de factura

--- src/main/java/mx/com/ironroller/controller/FacturaController.java
+++ src/main/java/mx/com/ironroller/controller/FacturaController.java
@@ -3,10 +3,25 @@ package mx.com.ironroller.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FacturaController {
 	private final Logger log = LoggerFactory.getLogger(FacturaController.class);
 	
+	@GetMapping("/facturas")
+	public String facturas() {
+		log.info("Regresando factura page");
+		return "factura/factura";
+	}
 	
+	@PostMapping("/facturas/upload")
+	public String procesarFactura(@RequestParam("invoiceFile") MultipartFile invoiceFile, RedirectAttributes redirectAttributes) {
+		log.info("procesar xml y agregar addenda");
+		return "redirect:/facturas";
+	}
 }
